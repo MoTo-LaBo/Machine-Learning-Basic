dee043b02215e68ce79366c8c5f2af92a92a86e4
MoTo <moto.labo.desgin@gmail.com>
2021-10-11 18:43:27 +0900

    vector

--- script/aivis.py
+++ script/aivis.py
@@ -21,7 +21,8 @@ def rename_func(p, idx: int, str1, str2):
 def rename_func_num(p, idx: int, str1, str2):
     sub_p = list(p.iterdir())[idx] # subpath　rename したい folder　選択
     tf = list(sub_p.glob('*'))     # target_folder　内の file path取得
-    for f in tf[1:]:                   # for文で元の path を使用して振り分けと　rename　を同時に行う
+    # for f in tf[1:]:                   # for文で元の path を使用して振り分けと　rename　を同時に行う
+    for f in tf:                   # vs code jupyter では index の slice は必要ない
         _, n = os.path.split(f)    # split で folder, fileに分ける
         r = re.sub(str1, str2, n)  # str1 削除したい数字　/ str2 代入したい文字列
         os.rename(f, r)
